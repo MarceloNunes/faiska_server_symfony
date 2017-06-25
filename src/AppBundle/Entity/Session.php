0aedfa85657cff98481d6199bb5162bd33475559
Marcelo Nunes <marcelo@marcelonunes.com.br>
2017-06-25 13:34:02 -0400

    adding Entity\Session and updating Entity\User to include active and birthDate

---
+++ src/AppBundle/Entity/Session.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="session")
+ */
+class Session
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $authKey;
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="sessions")
+     * @var User
+     */
+    protected $user;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $remoteAddress;
+    /**
+     * @ORM\Column(type="datetime")
+     * @var DateTime
+     */
+    protected $createdAt;
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    protected $active = true;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+    /**
+     * @param string $authKey
+     * @return Session
+     */
+    public function setAuthKey($authKey)
+    {
+        $this->authKey = $authKey;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     * @return Session
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRemoteAddress()
+    {
+        return $this->remoteAddress;
+    }
+
+    /**
+     * @param string $remoteAddress
+     * @return Session
+     */
+    public function setRemoteAddress($remoteAddress)
+    {
+        $this->remoteAddress = $remoteAddress;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     * @return Session
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param bool $active
+     * @return Session
+     */
+    public function activate($active)
+    {
+        $this->active = true;
+        return $this;
+    }
+
+    /**
+     * @param bool $active
+     * @return Session
+     */
+    public function deactivate($active)
+    {
+        $this->active = false;
+        return $this;
+    }
+}
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
 * @ORM\Entity
@@ -11,40 +13,50 @@ use Doctrine\ORM\Mapping as ORM;
 class User
 {
     /**
-    * @ORM\Column(type="integer")
-    * @ORM\Id
-    * @ORM\GeneratedValue(strategy="AUTO")
-    */
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=256)
+     * @var string
      */
     private $email;
-
     /**
      * @ORM\Column(type="string", length=50)
+     * @var string
      */
     private $password;
-
     /**
      * @ORM\Column(type="string", length=256)
+     * @var string
      */
     private $name;
-
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var Date
+     */
+    private $birthDate = null;
     /**
      * @ORM\Column(type="boolean")
+     * @var bool
      */
     private $admin = false;
     /**
      * @ORM\Column(type="datetime")
+     * @var DateTime
      */
     private $createdAt;
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $active = true;
 
     /**
-     * Get id
-     *
-     * @return integer
+     * @return int
      */
     public function getId()
     {
@@ -52,22 +64,6 @@ class User
     }
 
     /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
      * @return string
      */
     public function getEmail()
@@ -76,22 +72,16 @@ class User
     }
 
     /**
-     * Set password
-     *
-     * @param string $password
-     *
+     * @param string $email
      * @return User
      */
-    public function setPassword($password)
+    public function setEmail($email)
     {
-        $this->password = $password;
-
+        $this->email = $email;
         return $this;
     }
 
     /**
-     * Get password
-     *
      * @return string
      */
     public function getPassword()
@@ -100,22 +90,16 @@ class User
     }
 
     /**
-     * Set name
-     *
-     * @param string $name
-     *
+     * @param string $password
      * @return User
      */
-    public function setName($name)
+    public function setPassword($password)
     {
-        $this->name = $name;
-
+        $this->password = $password;
         return $this;
     }
 
     /**
-     * Get name
-     *
      * @return string
      */
     public function getName()
@@ -124,51 +108,101 @@ class User
     }
 
     /**
-     * Set admin
-     *
-     * @param boolean $admin
-     *
+     * @param string $name
      * @return User
      */
-    public function setAdmin($admin)
+    public function setName($name)
     {
-        $this->admin = $admin;
-
+        $this->name = $name;
         return $this;
     }
 
     /**
-     * Get admin
-     *
-     * @return boolean
+     * @return mixed
      */
-    public function getAdmin()
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * @param mixed $birthDate
+     * @return User
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin()
     {
         return $this->admin;
     }
 
     /**
-     * Set createdAt
-     *
-     * @param \DateTime $createdAt
-     *
+     * @return User
+     */
+    public function setAdmin()
+    {
+        $this->admin = true;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function unsetAdmin()
+    {
+        $this->admin = false;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
      * @return User
      */
     public function setCreatedAt($createdAt)
     {
         $this->createdAt = $createdAt;
-
         return $this;
     }
 
     /**
-     * Get createdAt
-     *
-     * @return \DateTime
+     * @return bool
      */
-    public function getCreatedAt()
+    public function isActive()
     {
-        return $this->createdAt;
+        return $this->active;
+    }
+
+    /**
+     * @return User
+     */
+    public function activate()
+    {
+        $this->active = true;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function deactivate()
+    {
+        $this->active = false;
+        return $this;
     }
 
     public function toArray()
